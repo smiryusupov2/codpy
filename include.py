cd8cd729300ec4ce8e3783984822f338e907eea5
codpy2020 <67156845+codpy2020@users.noreply.github.com>
2024-02-20 12:01:48 +0100

    rm

--- include.py
+++
@@ -1,6 +0,0 @@
-#######codpy import #######
-from codpy import *
-#######global variables#######
-parent_path = os.path.dirname(__file__)
-if parent_path not in sys.path: sys.path.append(parent_path)
-#######################################
